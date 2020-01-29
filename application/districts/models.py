05d8e012b552eed937c9de92897d6ea3b44997e2
noorarytila <rytilanoora@gmail.com>
2020-01-29 15:54:03 +0200

    juuh

--- application/districts/models.py
+++ application/districts/models.py
@@ -1,6 +1,8 @@
 from application import db
 
 class District(db.Model):
+
+    __tablename__= "district"
     id = db.Column(db.Integer, primary_key=True)
     date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
     date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
