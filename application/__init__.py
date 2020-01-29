9eab423f5a5c5cbd9c1874f39d2eceb5217fb62d
noorarytila <rytilanoora@gmail.com>
2020-01-29 15:28:51 +0200

    blööh

--- application/__init__.py
+++ application/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_migrate import Migrate
+#from flask_migrate import Migrate
 app = Flask(__name__)
 
 from flask_sqlalchemy import SQLAlchemy
@@ -15,7 +15,7 @@ else:
 
 
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+# migrate = Migrate(app, db)
 
 from application import views
 
@@ -27,9 +27,9 @@ from application.districts import views
 from application.auth import models
 from application.auth import views
 
-from application.auth.models import User
+# from application.auth.models import User
 
-from os import urandom
+""" from os import urandom
 app.config["SECRET_KEY"] = urandom(32)
 
 from flask_login import LoginManager
@@ -41,10 +41,14 @@ login_manager.login_message = "Please login"
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(user_id)
+    return User.query.get(user_id) """
 
 
-db.create_all()
+try:
+    db.create_all()
+
+except:
+    pass
 
 
 
