b8586cc124aafa0bd930ed8d86809b504e04cda1
noorarytila <rytilanoora@gmail.com>
2020-01-29 14:51:13 +0200

    säätöä..

--- application/migrations/env.py
+++ application/migrations/env.py
@@ -22,8 +22,7 @@ logger = logging.getLogger('alembic.env')
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 from flask import current_app
-config.set_main_option(SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URI']
-    )
+config.set_main_option(config.set_main_option( 'sqlalchemy.url', current_app.config.get( 'SQLALCHEMY_DATABASE_URI').replace('%', '%%')))
 target_metadata = current_app.extensions['migrate'].db.metadata
 
 # other values from the config, defined by the needs of env.py,
