46bbaf691f8e4589449d117abe165f850a1d86f5
noorarytila <rytilanoora@gmail.com>
2020-01-30 19:47:54 +0200

    kadun lisääminen korjattu

--- application/streets/forms.py
+++ application/streets/forms.py
@@ -1,12 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, validators, SelectField
 from wtforms.ext.sqlalchemy.fields import QuerySelectField
-#from application.districts.models import District
+from application.districts.models import District
 from application.streets.models import Street
 
 class StreetForm(FlaskForm):
 	name = StringField("Street name", [validators.Length(min=5)])
-	
 	districts_database = District.query.with_entities(District.id, District.name)
 	districts_field = [(x.id, x.name) for x in districts_database]
 
